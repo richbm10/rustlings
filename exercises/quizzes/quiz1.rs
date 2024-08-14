360997904eb09266dc186e470677d7c339cfd234
richbm10 <ricardo.bonilla.morales@gmail.com>
2024-08-13 22:09:38 -0600

    [QUIZ-01]: Finished first Rust quiz for: variables, functions, and if expressions

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(quantity: u32) -> u32 { 
+    if quantity > 40 {
+        return quantity;
+    }
+
+    quantity * 2
+ }
 
 fn main() {
     // You can optionally experiment here.
